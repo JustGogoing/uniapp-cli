559f356e5fc024dcf7ae46ac9ffc8d81ad113ad1
苍山暮色烟雨迟 <14536438@qq.com>
2020-08-11 23:32:01 +0800

    feat: 配置vuex模块

--- store/index.js
+++ store/index.js
@@ -1,1 +1,14 @@
+/**
+ * vuex基本配置
+ */
 import Vue from 'vue';
+import Vuex from 'vuex';
+import userModel from './modules/user/index.js';
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  namespace: true,
+  module: {
+    userModel
+  }
+})
